fe75f390cf482cb2bf3a99c086da05e51e351ca4
Kyrillos Sawiris <kyrillos.sawiris99@gmail.com>
2022-08-15 02:17:24 +0200

    modify LIFO Driver

--- Unit_4 System Architect/1- Data Structure/LIFO_DRIVER/main.c
+++ Unit_4 System Architect/1- Data Structure/LIFO_DRIVER/main.c
@@ -12,7 +12,7 @@
 int main()
 {
 	LIFO_Buf_t buffer1,buffer2;
-    int static_buf [LIFO_LENGHT];
+	int static_buf [LIFO_LENGHT];
 	buffer1.base = static_buf;
 	buffer1.head = buffer1.base;
 	buffer2.base = (int *) malloc(LIFO_LENGHT * sizeof(int));
@@ -23,37 +23,53 @@ int main()
 	printf("Buffer 1 (static allocation) LIFO_PUSH_ITEMS \n");
 	for(int i =0 ;i< 5;i++)
 	{
-		printf("%d\t", i);
-		LIFO_AddItem(&buffer1 , i);
+		if(LIFO_AddItem(&buffer1 , i) == LIFO_NO_ERROR)
+			printf("Writing %d in LIFO_uart passed\n",i);
+		else
+			printf("Writing %d in LIFO_uart failed\n",i);
 	}
 	fflush(stdout);
 	printf("\nBuffer 1 (static allocation) LIFO_POP_ITEMS \n");
 	int temp;
-		for(int i =0 ;i< 5;i++)
+	for(int i =0 ;i< 5;i++)
+	{
+		if(LIFO_PopItem(&buffer1 , &temp)==LIFO_NO_ERROR)
 		{
-			LIFO_PopItem(&buffer1 , &temp);
-			printf("%d\t", temp);
+			printf("Reading %d in static Buffer passed\n",temp);
 		}
-
-
-		printf("\n\n\===========================================================\n");
-
-		buffer2.count = 0;
-		buffer2.length = LIFO_LENGHT;
-		printf("Buffer 2 (Dynamic allocation) LIFO_PUSH_ITEMS \n");
-		for(int i =0 ;i< 5;i++)
+		else
 		{
-			printf("%d\t", i);
-			LIFO_AddItem(&buffer1 , i);
+			printf("Reading %d in static Buffer failed\n",temp);
 		}
-		fflush(stdout);
-		printf("\nBuffer 2 (Dynamic allocation) LIFO_POP_ITEMS \n");
+	}
 
-			for(int i =0 ;i< 5;i++)
-			{
-				LIFO_PopItem(&buffer1 , &temp);
-				printf("%d\t", temp);
-			}
+
+	printf("\n\n\===========================================================\n");
+
+	buffer2.count = 0;
+	buffer2.length = LIFO_LENGHT;
+	printf("Buffer 2 (Dynamic allocation) LIFO_PUSH_ITEMS \n");
+	for(int i =0 ;i< 5;i++)
+	{
+		if(LIFO_AddItem(&buffer1 , i) == LIFO_NO_ERROR)
+			printf("Writing %d in Dynamic Buffer passed\n",i);
+		else
+			printf("Writing %d in Dynamic Buffer failed\n",i);
+	}
+	fflush(stdout);
+	printf("\nBuffer 2 (Dynamic allocation) LIFO_POP_ITEMS \n");
+
+	for(int i =0 ;i< 5;i++)
+	{
+		if(LIFO_PopItem(&buffer1 , &temp)==LIFO_NO_ERROR)
+		{
+			printf("Reading %d in Dynamic Buffer passed\n",temp);
+		}
+		else
+		{
+			printf("Reading %d in Dynamic Buffer failed\n",temp);
+		}
+	}
 
 
 
